e66c02bc8120cbab496b842a5bc06a758dede216
Jose Breiner <jbpaig@gmail.com>
2022-01-22 17:54:09 -0500

    classic for

--- src/05/for.go
+++ src/05/for.go
@@ -7,18 +7,43 @@ import (
 
 func main() {
 
-	var c string = ""
+	/* 	
+		var c string = ""
 
-	for i := 0; i < 20; i++ {
-		if i % 2 == 0 {
-			c += strconv.Itoa(i)+"\n"
+		for i := 0; i < 20; i++ {
+			if i % 2 == 0 {
+				c += strconv.Itoa(i)+"\n"
+			}
+
+			if i == 9 {
+				break
+			}
 		}
 
-		if i == 9 {
-			break
+		fmt.Printf("%s",c)
+
+	*/
+
+	números := [3][2]int{}
+	valor := 0
+
+
+	for i := 0; i < len(números); i++ {
+		for j := 0; j < len(números[i]); j++ {
+			valor++ 
+			números[i][j] = valor
 		}
 	}
 
-	fmt.Printf("%s",c)
+	c := ""
 
+	for i := 0; i < len(números); i++ {
+		c+="[ "
+		for j := 0; j < len(números[i]); j++ {
+			c += strconv.Itoa(números[i][j])+" "
+		}
+		c += "]\n"
+	}
+
+	fmt.Printf("%s", c)
 }
